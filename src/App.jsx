51a287fe9f09ed453885dd1b303fc73fb9bb09c8
mfardilam <mfardilam@correo.udistrital.edu.co>
2023-10-30 18:45:49 -0500

    Estructura inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './Components/Card'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Formulario</h1>
+    <form></form>
+      <Card></Card>
+    </>
+  )
+}
+
+export default App
